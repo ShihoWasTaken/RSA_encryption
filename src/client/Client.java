5b60da7fb14705c20e20e15f516a5d21d130e4f2
adrien3 <adrien.caselles@gmail.com>
2017-01-31 16:25:31 +0100

    Suppression des warning

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,13 +1,9 @@
 package client;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.net.InetAddress;
 import java.net.Socket;
 
 import javax.swing.JFrame;
